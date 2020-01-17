1cf4dcd07088a3d8ea5457009025211fbf07ed0b
unsong0316 <uschoe1994@gmail.com>
2020-01-17 14:49:24 +0900

    01171450

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hi!");
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
+const _webPort = 80;
+global.app = express();
+app.use(cors());
+global.router = express.Router();
+global.path = __dirname;
+app.use(bodyParser.json());
+app.use("/", router);
+
+router.get("/", (req, res) => {
+    console.log(path);
+    res.sendFile(`${path}/web-client/index.html`);
+});
+
+router.get("/app.js", (req, res) => {
+    res.sendFile(`${path}/web-client/app.js`);
+});
+app.listen(_webPort, () => {
+    console.log(`web server running on ${_webPort}`);
+});
