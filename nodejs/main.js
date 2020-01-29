614f09c608361bb080d531ea77208ad512611f3c
unsong0316 <uschoe1994@gmail.com>
2020-01-29 17:15:35 +0900

    app5

--- nodejs/main.js
+++ nodejs/main.js
@@ -13,6 +13,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>
@@ -42,7 +43,7 @@ var app = http.createServer(function(request,response){
           var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
-        })
+        });
       } else {
         fs.readdir('../main', function(error, filelist){
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
@@ -54,11 +55,44 @@ var app = http.createServer(function(request,response){
           });
         });
       }
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
-    }
- 
+    } else if(pathname === '/create'){
+      fs.readdir('../main', function(error, filelist){
+        var title = 'WEB - create';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `
+          <form action="http://localhost:3000/process_create" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `);
+        response.writeHead(200);
+        response.end(template);
+      });
+      }else if(pathname === '/create_process'){
+        var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            })
+        });
+      } else {
+        response.writeHead(404);
+        response.end('Not found');
+      }
+  });
+  app.listen(3000);
  
  
 });
