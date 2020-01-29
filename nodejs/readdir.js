4642b03c36ae18d4c1756d0fef421b9f82686bd8
unsong0316 <uschoe1994@gmail.com>
2020-01-29 12:28:57 +0900

    app4

--- nodejs/readdir.js
+++ nodejs/readdir.js
@@ -1,4 +1,6 @@
+var testFolder = '../main/';
 var fs = require('fs');
-fs.readFile('sample.txt', 'utf8', function(err, data){
-  console.log(data);
-});
+ 
+fs.readdir(testFolder, function(error, filelist){
+  console.log(filelist);
+})
